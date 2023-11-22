5db1e8c967f2b9ef3d35bc8e17a8c5dbf3a53fd8
JJARSANGKIM <kjanicat@gmail.com>
2023-11-23 02:17:31 +0900

    feat : Player Scripts PlayerAnimation Update (#46)

---
+++ PPONGARI/Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    private Animator animator;
+    PlayerMovement playerMovement;
+    Rigidbody2D rigid2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigid2D = GetComponent<Rigidbody2D>();
+        playerMovement = GetComponent<PlayerMovement>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    public void Move()
+    {
+        if (Input.GetButton("Horizontal"))
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else 
+        { 
+            animator.SetBool("isWalking", false); 
+        }
+    }
+
+    public void Jump()
+    {
+        if (playerMovement.isLongJump && rigid2D.velocity.y > 0)
+        {
+            animator.SetBool("isJump", true);
+            Debug.Log("Jump");
+        }
+        else
+        { 
+            animator.SetBool("isJump", false); 
+        }
+    }
+
+}
